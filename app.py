d8fca2a7a50af6e57fa3b570526f6441fe58358f
Azula21 <janelinw@usc.edu>
2020-05-05 18:13:59 -0700

    Make endpoint pretty

--- app.py
+++ app.py
@@ -18,7 +18,21 @@ def lookfor():
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return """
+    <!DOCTYPE html>
+<head>
+   <title>Endpoint</title>
+   <link rel="stylesheet" href="http://stash.compjour.org/assets/css/foundation.css">
+</head>
+<body style="width: 880px; margin: auto;">  
+    <h1>Welcome to a kid-friendly zone</h1>
+    <p>This is our endpoint for GET requests</p>
+    <p>And here's an image:</p>
+    <a href="https://www.flickr.com/photos/81191171@N04/8402256318">
+        <img src="https://live.staticflickr.com/8189/8402256318_775f7567b9_z.jpg" alt="Oh how cute">
+    </a>
+</body>
+"""
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
